416c2d18609e7b5813f27a8b9cabe73c3631b460
Kariniii <love_karino444ka@mail.ru>
2025-05-21 13:40:00 +0300

    main.c

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
+#include "filesystem.h"
+#include <string.h>
 
+int main() {
+    FILE* fs = fs_open("disk.simplefs");
+    if (!fs) return 1;
+
+    char cmd[20], filename[100], content[1024];
+
+    printf("SimpleFS Shell. Commands: create, view, modify, delete, exit\n");
+    while (1) {
+        printf("> ");
+        scanf("%s", cmd);
+if (strcmp(cmd, "create") == 0) {
+            scanf("%99s %1023[^\n]", filename, content);
+            fs_create(fs, filename, content);
+        }
+        else if (strcmp(cmd, "view") == 0) {
+            scanf("%99s", filename);
+            fs_view(fs, filename);
+        }
+        else if (strcmp(cmd, "modify") == 0) {
+            scanf("%99s %1023[^\n]", filename, content);
+            fs_modify(fs, filename, content);
+        }
+        else if (strcmp(cmd, "delete") == 0) {
+            scanf("%99s", filename);
+            fs_delete(fs, filename);
+        }
+        else if (strcmp(cmd, "exit") == 0) break;
+        else printf("Unknown command\n");
+    }
+
+    fs_close(fs);
+    return 0;
+}
