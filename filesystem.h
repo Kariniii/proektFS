485cf5c657c6de8cd666e4ab895e226aeab1fc23
Kariniii <love_karino444ka@mail.ru>
2025-05-21 13:27:33 +0300

    filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,1 +1,21 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
 
+#include <stdio.h>
+
+// Открыть или создать файловую систему
+FILE* fs_open(const char* filename);
+
+// Просмотреть содержимое файла
+void fs_view(FILE* fs, const char* filename);
+
+// Создать новый файл
+void fs_create(FILE* fs, const char* filename, const char* content);
+
+// Изменить файл
+void fs_modify(FILE* fs, const char* filename, const char* new_content);
+
+// Удалить файл
+void fs_delete(FILE* fs, const char* filename);
+
+#endif
